9ba8801d3afc0a352ca4603797edb19095e888b9
Dhaval Barot <dhaval.barot@rapidops.com>
2018-12-04 11:53:34 +0530

    - Project creation.

---
+++ iOSDevOps/iOSDevOps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDevOps
+//
+//  Created by Dhaval Barot on 04/12/18.
+//  Copyright © 2018 Dhaval Barot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ iOSDevOps/iOSDevOpsTests/iOSDevOpsTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOSDevOpsTests.swift
+//  iOSDevOpsTests
+//
+//  Created by Dhaval Barot on 04/12/18.
+//  Copyright © 2018 Dhaval Barot. All rights reserved.
+//
+
+import XCTest
+@testable import iOSDevOps
+
+class iOSDevOpsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
